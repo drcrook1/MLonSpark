0d28b0b91f803e56fffac068318091af8bc87897
drcrook1 <drcrook@outlook.com>
2017-04-18 14:02:00 -0400

    updated to install into virtual environments for anaconda

--- InstallScripts/InstallADLSPackages.sh
+++ InstallScripts/InstallADLSPackages.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
+ANACONDA_BASEPATH="/usr/bin/anaconda"
 
+# install into python 2.7
+source "$ANACONDA_BASEPATH/bin/activate"
 pip install azure-mgmt-resource
 pip install azure-mgmt-datalake-store
 pip install azure-datalake-store
+
+#install into python 3.5
+source "$ANACONDA_BASEPATH/bin/activate" py35
+pip install azure-mgmt-resource
+pip install azure-mgmt-datalake-store
+pip install azure-datalake-store
+
+#Deactivate
+source "$ANACONDA_BASEPATH/bin/deactivate"
