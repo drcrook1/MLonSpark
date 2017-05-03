f13cc9288b5853446f177a239747f0c640e910ae
drcrook1 <drcrook@outlook.com>
2017-05-03 16:07:34 -0400

    add plotly installer

--- InstallScripts/InstallPlottingLibs.sh
+++ InstallScripts/InstallPlottingLibs.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 ANACONDA_BASEPATH="/usr/bin/anaconda"
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
 # install into python 2.7
 source "$ANACONDA_BASEPATH/bin/activate"
-pip install pydoop --upgrade
+pip install plotly --upgrade
 
 #install into python 3.5
 source "$ANACONDA_BASEPATH/bin/activate" py35
-pip install pydoop --upgrade
+pip install plotly --upgrade
 
 #Deactivate
 source "$ANACONDA_BASEPATH/bin/deactivate"
